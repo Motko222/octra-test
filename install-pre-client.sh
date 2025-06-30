49fa14f3f8e02e32a1e08189b7ce668a0293fb5d
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-30 13:28:06 +0200

    Create install-pre-client.sh

---
+++ install-pre-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /root
+mkdir octra
+git clone https://github.com/octra-labs/octra_pre_client.git
+cd octra_pre_client
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
