a578c6220ff8bbb3a52140ec44be33a7406e65e7
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-28 16:01:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-apt install python3.10-venv
-
 cd /root
-git clone https://github.com/octra-labs/octra_pre_client.git
-cd octra_pre_client
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-deactivate
+git clone https://github.com/octra-labs/ocs01-test.git
+cd ocs01-test
+cargo build --release
+cp EI/exec_interface.json .
+cp target/release/ocs01-test .
+cp /root/octra-pre-client/wallet.json .
